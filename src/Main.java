3451dbe0fdf2092804bccb1d963db288d00e4d87
kokic <3250679033@qq.com>
2022-03-05 10:51:34 +0800

    # space

--- src/Main.java
+++ src/Main.java
@@ -4,47 +4,47 @@ import static quasi.Ind.index;
 
 public class Main {
 
-	public static final QuasiFunction.one_bool<Object> println = new QuasiFunction.one_bool<Object>() {
-		@Override
-		public boolean invoke(Object arg) {
-			System.out.println(arg);
-			return true;
-		}
-	};
-	
-	public static final QuasiFunction.any_void foreach = args -> {
-		QuasiExpress.pass(index.clear());
-		while ( QuasiExpress.keep(index.less(args.length)) 
-		  &&    QuasiExpress.keep(index.println.invoke(args))
-		  &&    QuasiExpress.pass(index.increase()) ) 
-		{}
-	};
-	
-	
-	public static void main(String[] args)
-	{
-		System.out.println(">>> test: ");
-		QuasiFunction.invokeUniversal(println, "good");
-		
-		System.out.println(">>> foreach: ");
-		// QuasiFunction.invokeUniversal(foreach, 1, 5, 3);
-		foreach.invoke((Object[]) Main.class.getMethods());
-		QuasiFunction.invokeUniversal(foreach, (Object[]) Main.class.getMethods());
-		
-		// int value = 2; 
-		
-		/* 
-		coodition ? left : right;
+    public static final QuasiFunction.one_bool<Object> println = new QuasiFunction.one_bool<Object>() {
+        @Override
+        public boolean invoke(Object arg) {
+            System.out.println(arg);
+            return true;
+        }
+    };
+    
+    public static final QuasiFunction.any_void foreach = args -> {
+        QuasiExpress.pass(index.clear());
+        while ( QuasiExpress.keep(index.less(args.length)) 
+          &&    QuasiExpress.keep(index.println.invoke(args))
+          &&    QuasiExpress.pass(index.increase()) ) 
+        {}
+    };
+    
+    
+    public static void main(String[] args)
+    {
+        System.out.println(">>> test: ");
+        QuasiFunction.invokeUniversal(println, "good");
+        
+        System.out.println(">>> foreach: ");
+        // QuasiFunction.invokeUniversal(foreach, 1, 5, 3);
+        foreach.invoke((Object[]) Main.class.getMethods());
+        QuasiFunction.invokeUniversal(foreach, (Object[]) Main.class.getMethods());
+        
+        // int value = 2; 
+        
+        /* 
+        coodition ? left : right;
 
-		QuasiExpress.expr((coodition
-		    && QuasiExpress.pass(left))
-			|| QuasiExpress.pass(right)); 
-		*/
+        QuasiExpress.expr((coodition
+            && QuasiExpress.pass(left))
+            || QuasiExpress.pass(right)); 
+        */
 
-		/* QuasiExpress.expr((5 > 3
-			&& QuasiExpress.pass(++value)) 
-			|| QuasiExpress.pass(--value)); */
-		
-		// System.out.println("value: " + value);
-	}
+        /* QuasiExpress.expr((5 > 3
+            && QuasiExpress.pass(++value)) 
+            || QuasiExpress.pass(--value)); */
+        
+        // System.out.println("value: " + value);
+    }
 }
